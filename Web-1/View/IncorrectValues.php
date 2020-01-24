94a73ddb9d04d59dd400823cfddfa22e046b243d
BogdanNovikov <44485166+BogdanNovikov@users.noreply.github.com>
2020-01-24 22:15:13 +0100

    new css
    changed incorrectvalues.php and estilos.css

--- Web-1/View/IncorrectValues.php
+++ Web-1/View/IncorrectValues.php
@@ -2,13 +2,18 @@
 <head>
     <meta charset="UTF-8">
     <link rel="icon" type="image/x-icon" href="Pictures/logo.ico">
-    <link rel="stylesheet" type="text/css" href=CSS/estilos.css>
+    <link rel="stylesheet" type="text/css" href="../CSS/estilos.css">
 </head>
-<body>
+<body style="background-image: url('../Pictures/bg1.jpg'); background-repeat: no-repeat;background-position: center;background-size: 100% 75%;">
 <?php include "MenuOption.php";
 include "header.php";?>
 
+<div class='box'>
 <p>You are not registered.</p>
-<a href="index.php?accio=registre" >Sign Up!</button></a>
-
+<a class="a" href="index.php?accio=registre" >Sign Up!</a>
+</div>
+</body>
 </html>
+
+
+
